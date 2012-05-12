c2237228518a33ac263b13490a8ba9a009b2c178
Pat George <pat.george@gmail.com>
2012-05-11 22:35:03 -0400

    Removed unneeded code.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,10 +1,5 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    # @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    # @window.rootViewController = PtTimerController.alloc.init
-    # @window.rootViewController.wantsFullScreenLayout = true
-    # @window.makeKeyAndVisible
-    # true
     nav = UINavigationController.alloc.initWithRootViewController(PtTimerController.alloc.init)
     nav.wantsFullScreenLayout = true
     nav.toolbarHidden = true
